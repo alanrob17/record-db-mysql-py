8989368f9605a5d2ceacd95b4d9d1e5cc06577da
Alan Robson <alanr@live.com.au>
2023-11-04 10:38:46 +1100

    Update Sql stroed procedures.

--- record-db-sps.sql
+++ record-db-sps.sql
@@ -1,4 +1,54 @@
 DELIMITER $
+
+CREATE PROCEDURE CreateArtist(
+    IN _firstName VARCHAR(255),
+    IN _lastName VARCHAR(255),
+    IN _biography TEXT
+)
+BEGIN
+    DECLARE _artistName VARCHAR(255);
+
+    IF _firstName IS NULL OR _firstName = '' THEN
+        SET _artistName = _lastName;
+    ELSE
+        SET _artistName = CONCAT(_firstName, ' ', _lastName);
+    END IF;
+
+    INSERT INTO Artist (FirstName, LastName, Name, Biography)
+    VALUES (_firstName, _lastName, _artistName, _biography);
+
+    SELECT LAST_INSERT_ID() AS ArtistId;
+END $
+
+DELIMITER ;
+
+DELIMITER $
+
+CREATE PROCEDURE CreateRecord(
+_artistId int,
+_name varchar(100),
+_field varchar(50),
+_recorded int,
+_label varchar(50),
+_pressing varchar(50),
+_rating varchar(4),
+_discs int,
+_media varchar(50),
+_bought datetime,
+_cost decimal(10,4),
+_review longtext
+)
+BEGIN
+    INSERT INTO Record (artistId, name, field, recorded, label, pressing, rating, discs, media, bought, cost, review)
+    VALUES (_artistId, _name, _field, _recorded, _label, _pressing, _rating, _discs, _media, _bought, _cost, _review);
+
+    SELECT LAST_INSERT_ID() AS RecordId;
+END $
+
+DELIMITER ;
+
+DELIMITER $
+
 CREATE PROCEDURE UpdateRecordById(
     IN _recordId INT,
 	IN _artistId INT,
@@ -12,31 +62,31 @@ CREATE PROCEDURE UpdateRecordById(
     IN _media VARCHAR(255),
     IN _bought VARCHAR(255),
     IN _cost VARCHAR(255),
-    IN _coverName VARCHAR(255),
-    IN _review TEXT,
-	IN _freeDbId INT
-	
+    IN _review TEXT
 )
 BEGIN
     UPDATE Record
     SET artistId = _artistId, name = _name, field = _field, recorded = _recorded,
         label = _label, pressing = _pressing, rating = _rating,
         discs = _discs, media = _media, bought = _bought,
-        cost = _cost, coverName = null, review = _review, freeDbId = null
+        cost = _cost, review = _review
     WHERE
-        recordId = _recordId;
+        RecordId = _recordId;
 END $
+
 DELIMITER ;
 
 
 
 DELIMITER $
-CREATE PROCEDURE DeleteRecordById(
+ 
+ CREATE PROCEDURE DeleteRecordById(
     IN _recordId INT
 )
 BEGIN
 	DELETE FROM Record WHERE RecordId = _recordId;
 END $
+
 DELIMITER ;
 
 
@@ -52,4 +102,28 @@ END $
 DELIMITER ;
 
 
-CALL GetRecordByName('Cutting Edge');
+DELIMITER $
+
+CREATE PROCEDURE GetArtistIdByNames(
+IN _firstName VARCHAR(255), 
+IN _lastName VARCHAR(255)
+)
+BEGIN
+    SELECT ArtistId FROM Artist WHERE FirstName = _firstName AND LastName = _lastName;
+END $
+
+DELIMITER ;
+
+
+DELIMITER $
+
+CREATE PROCEDURE GetRecordById(
+    IN _recordId INT
+)
+BEGIN
+    SELECT recordId, artistId, name, field, recorded, label, pressing, rating, discs, media, bought, cost, review
+    FROM Record
+    WHERE recordId = _recordId;
+END $
+
+DELIMITER ;
